0a7172198a67eac540fc8a882ee687dffb3ec81f
hectorj <hector.jusforgues@gmail.com>
2023-10-08 20:50:39 +0200

    improve README

---
+++ README.md
@@ -0,0 +1,1 @@
+See [./postgrecho/README.go](./postgrecho/README.go)
--- postgrecho/README.go
+++ postgrecho/README.go
@@ -5,8 +5,10 @@
 // - postgrecho does not have a global state, which means you can start one server for each test and run them in parallel (though if you overdo it your computer might run out of resources)
 // - postgrecho supports concurrent queries to the same server (though it does cheat a bit by stalling out-of-order queries)
 //
-// postgrecho is not recommended for:
-// - performance testing
-// - driver testing
-// - race condition testing (unless you enable the strict mode, which forces you to always run your query in the same order, effectively eliminating database race conditions)
+// postgrecho is recommended when:
+// - you want to test your SQL queries but you don't want to spin up a Postgresql Server in your CI nor when you make a non-SQL related change
+//
+// postgrecho is not recommended when:
+// - testing performances (the proxy may add a non-constant overhead in recording mode, and the original query latency is not replayed)
+// - testing database race conditions (unless you enable the strict mode, which forces you to always run your queries in the same order, effectively eliminating database race conditions by forbidding concurrency)
 package postgrecho
